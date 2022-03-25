80211f8651b02b905ef6120b9e2573d5a01e3121
Manniwebchefz <102353844+Manniwebchefz@users.noreply.github.com>
2022-03-25 14:30:21 +0530

    Adding API
    Adding API

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?convert=BRL&limit=5000&start=1',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+echo '<pre>';
+print_r($response)
+echo '</pre>';
+curl_close($curl);
